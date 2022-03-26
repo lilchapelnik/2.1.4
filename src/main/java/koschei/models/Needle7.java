71849a1c719f730c8fd38760a39ca8e83ffadb9a
lilldd <d073198@gmail.com>
2022-03-26 22:53:23 +0700

    step two

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,7 +1,16 @@
 package koschei.models;
 
-public class Needle7 {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Needle7 {
+    private Deth8 death;
+
+    @Autowired
+    public Needle7(Deth8 death) {
+        this.death = death;
+    }
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
